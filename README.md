1dff69ba459a448598bfc2b235f68d4308ff3036
dalyl <adidadi.adiden@gmail.com>
2024-02-02 00:49:24 +0100

    v1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tprep
 Script for commenting out Angular 17's new control flow so that translation keys can be extracted using transloco-keys-manager v3.8.0
+
+## Usage
+
+1. `tprep [path-to-html] -c` to comment
+2. then use transloco-keys-manager to extract your keys
+3. `tprep [path-to-html] -u` to uncomment
+4. Don't forget to reformat your file :)
---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import * as prettier from "prettier";
+import * as fs from "fs";
+
+const program = new Command();
+
+program.argument("<filepath>", "HTML file path")
+    .option("-c --comment", "Comment the new Angular 17 control flow")
+    .option("-u --uncomment", "Uncomment the new Angular 17 control flow")
+    .parse(process.argv);
+
+const filepath = program.args[0];
+const commentOption = program.opts().comment;
+const uncommentOption = program.opts().uncomment;
+
+function validateCommands()
+{
+    if (commentOption === uncommentOption) {
+        program.error("error: please provide either the comment option -c or the uncomment option -u");
+    }
+
+    if (!filepath.endsWith(".html")) {
+        program.error("error: please choose an HTML file");
+    }
+
+    if (!fs.existsSync(filepath)) {
+        program.error("error: file does not exist");
+    }
+}
+
+async function parseFile() {
+    const file = fs.readFileSync(filepath, 'utf8');
+    const formattedFile = await prettier.format(file, {parser: "angular"});
+    fs.writeFileSync(filepath, formattedFile, 'utf8');
+    const lines = formattedFile.split('\n');
+
+    let previousLeftIndent = -1;
+    const commentRegex = /<!--[^-]*[@}][^-]*-->/;
+    for (let i = 0; i < lines.length; ++i) {
+        // Comment
+        const currentLeftIndent = (lines[i].match(/^\s*/) || [])[0].length;
+        if (commentOption && lines[i].includes("@")) {
+            previousLeftIndent = currentLeftIndent;
+            lines[i] = `<!--${lines[i]}`;
+            lines[i] = `${lines[i]}-->`;
+        }
+        else if (commentOption && lines[i].includes("}") && currentLeftIndent === previousLeftIndent) {
+            previousLeftIndent = -1;
+            lines[i] = `<!--${lines[i]}`;
+            lines[i] = `${lines[i]}-->`;
+        }
+
+        // Uncomment
+        if (uncommentOption && commentRegex.test(lines[i])) {
+            lines[i] = lines[i].replace("<!--", "");
+            lines[i] = lines[i].replace("-->", "");
+        }
+    }
+    fs.writeFileSync(filepath, lines.join('\n'), 'utf8');
+}
+
+validateCommands();
+await parseFile();
